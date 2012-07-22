1f4cbfc7723e86016580851b230675538872138e
Shinpei Maruyama <shinpeim@gmail.com>
2012-07-22 15:40:30 +0900

    wrote document

--- lib/Acme/NyanDoku.pm
+++ lib/Acme/NyanDoku.pm
@@ -65,15 +65,25 @@ __END__
 
 =head1 NAME
 
-Acme::NyanDoku -
+Acme::NyanDoku - NyanDoku-ize your code
 
 =head1 SYNOPSIS
 
   use Acme::NyanDoku;
+  print "nyaaaaaaaaaaan!!!!";
 
 =head1 DESCRIPTION
 
-Acme::NyanDoku is
+The first time you run a program under use Acme::NyanDoku, the module rewrite your source file. like this:
+
+  use Acme::NyanDoku;
+  NyAn nyan nyan nYAN nyAn nYAN NyaN nYAn NYAn nYAn nYan nYAN nyan nyAn nyAn nyAn NYAn nYAn NyaN nYAN nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn nyaN nYAn NYAn nYAn nyaN nyAn nyaN nyAn nyaN nyAn nyaN nyAn nyAn nyAn NyAN nyAN NyAn nyan
+
+Of cause, you can execute NyanDoku-ized source.
+
+=head1 WHAT IS NYANDOKU
+
+"nyandoku" is a parody of "nandoku". "nandoku" means "obfuscation" in Japanese. "nyan" means "meow" in Japanese. YES, WE REALY LOVE CATS!!!!!!!
 
 =head1 AUTHOR
 
